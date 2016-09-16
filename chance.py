ad1db5e457eea09e09e01b06ba2cf02594921592
elarnia <slyswindler@gmail.com>
2016-09-16 14:19:19 -0400

    Add files via upload

---
+++ chance.py
@@ -0,0 +1,20 @@
+#!C:\Python34\python.exe
+
+## chance.py
+## A shell program for generating random numbers.
+## Author: Samuel Patrick
+## Date: 9/16/16
+
+import random
+
+def generator(x,y):
+	mynum = random.randint()
+	return mynum
+	
+	
+#__main__
+user,low,high,rand_num = -1
+
+rand_num = gener(low, high)
+
+print('This is random ... ',rand_num)
